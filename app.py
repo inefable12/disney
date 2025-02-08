7734bf2369cce2d13e2b980471a754d9cdb66fde
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-02-08 18:13:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import random
 
 def main():
+    st.set_page_config(page_title="Adivina la Película", page_icon="🎬")
     st.title("🎬 Adivina la Película con Emojis! 🍿")
-    st.write("Adivina el nombre de la película de Disney basándote en los emojis. ¡Buena suerte!")
+    st.write("Intenta adivinar el nombre de la película de Disney basándote en los emojis. ¡Buena suerte!")
 
     peliculas = {
         "🦁👑": "El Rey León",
@@ -17,20 +18,23 @@ def main():
         "👸🐸": "La Princesa y el Sapo",
         "🐉👦": "Mulan"
     }
-    
+
     opciones = list(peliculas.values())
-    respuestas = {}
-    
-    for emoji, pelicula_correcta in peliculas.items():
-        st.subheader(f"¿Qué película es? {emoji}")
-        opciones_mezcladas = random.sample(opciones, 4)
-        if pelicula_correcta not in opciones_mezcladas:
-            opciones_mezcladas[random.randint(0, 3)] = pelicula_correcta
+    respuestas_usuario = {}
+
+    with st.form("quiz_form"):
+        for emoji, pelicula_correcta in peliculas.items():
+            st.subheader(f"¿Qué película es? {emoji}")
+            opciones_mezcladas = random.sample(opciones, 4)
+            if pelicula_correcta not in opciones_mezcladas:
+                opciones_mezcladas[random.randint(0, 3)] = pelicula_correcta
+            
+            respuestas_usuario[emoji] = st.radio("Selecciona una opción:", opciones_mezcladas, key=emoji)
         
-        respuestas[emoji] = st.radio("Selecciona una opción:", opciones_mezcladas, key=emoji)
+        submit_button = st.form_submit_button("Ver resultado")
     
-    if st.button("Ver resultado"):
-        puntaje = sum(1 for emoji, pelicula_correcta in peliculas.items() if respuestas[emoji] == pelicula_correcta)
+    if submit_button:
+        puntaje = sum(1 for emoji, pelicula_correcta in peliculas.items() if respuestas_usuario[emoji] == pelicula_correcta)
         st.success(f"Tu puntaje: {puntaje}/10")
         
         if puntaje == 10:
